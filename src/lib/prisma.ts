186a2faa46400774aa98cb126cbe2ef0f38d6cfe
devCaiqueWS <dev_caiquews@outlook.com>
2025-11-20 16:35:08 -0300

    feat: v2.1.0

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const logConfig =
+const logConfig: Prisma.LogLevel[] =
   process.env.NODE_ENV === 'development'
     ? ['query', 'error', 'warn']
     : ['error']
